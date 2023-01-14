94c5b5ef729eeede85e310de63df28618efc3123
Ryan Valera <valeraryan@gmail.com>
2023-01-14 15:51:30 -0800

    Started adding basic content and styling to template.html file and main.css

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,10 @@ module.exports = {
         assetModuleFilename: "[name][ext]"
     },
     devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress:  true,
-        historyApiFallback: true,
-    },
     module: {  // module object
-        rules: [  // rules array for each file type (ex. scss)
-            { // add a test value as a regular expression (any files that end with this extension .scss)
-                test: /\.scss$/,
+        rules: [  // rules array for each file type (ex. css)
+            { // add a test value as a regular expression (any files that end with this extension .css)
+                test: /\.css$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'], // apply loaders
             },
             {
@@ -37,7 +27,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: '💩 To Doo-Doo List',
             filename: "index.html",
             template: "src/template.html"
         }),
